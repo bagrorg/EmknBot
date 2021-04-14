1a5ef63f74a2b04f19ce696f7e792482972568e5
bagrorg <kuldanvit@gmail.com>
2021-04-14 23:51:02 +0300

    +storage and user interaction trait

--- src/main/scala/com/emknbot/bot/Components.scala
+++ src/main/scala/com/emknbot/bot/Components.scala
@@ -1,12 +1,14 @@
 package com.emknbot.bot
 
 import java.time.ZonedDateTime
-
 import canoe.models.MessageEntity.Email
+import com.emknbot.bot.ComponentsAliases.ID
 import sun.security.util.Password
 
+object ComponentsAliases {
+  type ID = Int
+}
 
-case class User(name : String, email: Email, password: Password)
+case class User(name : String, email: Email, password: Password, id : ID)
 case class Event(description: String, dateTime : ZonedDateTime)
 case class Calendar(schedule: Map[ZonedDateTime, List[Event]])
-
