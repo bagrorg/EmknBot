e1e4569a88f64c29ef9953ba2966dfa391c78da4
bagrorg <kuldanvit@gmail.com>
2021-04-15 17:06:25 +0300

    + either on repositories

--- src/main/scala/com/emknbot/bot/Repositories/Repository.scala
+++ src/main/scala/com/emknbot/bot/Repositories/Repository.scala
@@ -2,5 +2,5 @@ package com.emknbot.bot
 
 trait Repository[F[_], Id, Data] {
   def save(data: Data) : F[Unit]
-  def findByID(id : Id) : F[Data]
+  def findByID(id : Id) : F[Either[String, Data]]
 }
